4a4d88c6f8ff0214e9cb2e922c0e807c9aa94256
drsalas_indra <drsalas@minsait.com>
2024-09-16 15:59:18 +0200

    compile more than 1

--- .github/actions/configure-git/git-diff.sh
+++ .github/actions/configure-git/git-diff.sh
@@ -50,7 +50,7 @@ for file in $CHANGED_FILES; do
           CURRENT_VERSION=$VERSION
         fi
         # Check if the binary name exists in the PR_NAME
-        if [[ "$PR" == "$PR_NAME" ]]; then
+        if [[ $PR == $PR_NAME ]]; then
           BINARY_EXISTS_PR=true
         fi
       fi
